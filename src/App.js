a33122e595a9bf77bccde3bc572d5d3e0095975b
Liam O'Shea <loshea@rolling-wave.com>
2021-11-06 19:27:58 -0300

    Test server in deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
 
 function App() {
+
+  const [msg, setMsg] = useState("Hello World")
+
+  useEffect(()=>{
+    async function callBackendAPI(){
+
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+    
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      
+      setMsg(body.express)
+
+    }
+
+    callBackendAPI()
+    
+  },[])
+
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {msg}
         </p>
         <a
           className="App-link"
